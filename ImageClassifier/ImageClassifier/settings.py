ca97ab5577621a333064f30cc7104a076b3bddfe
ashrotd <dhakalrabin04@gmail.com>
2020-10-31 22:53:30 +0545

    Image classfier

--- ImageClassifier/ImageClassifier/settings.py
+++ ImageClassifier/ImageClassifier/settings.py
@@ -37,16 +37,21 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
 
     'images',
     
     'rest_framework',
-    'corsheaders',
+    
+]
+CORS_ORIGIN_WHITELIST = [
+    "http://localhost:3000"
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
